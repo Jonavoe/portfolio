6f62ea28b9f72a3dfd1ce146fd963230c418771f
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-26 18:51:26 -0300

    Create Logo.tsx

---
+++ src/components/icons/Logo.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <svg
+      id="logo"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 84 96"
+    >
+      <title>Logo</title>
+      <g transform="translate(-8.000000, -2.000000)">
+        <g transform="translate(11.000000, 5.000000)">
+          <polygon
+            id="Shape"
+            fill="#0a192f"
+            stroke="#64ffda"
+            strokeWidth="5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            points="39 0 0 22 0 67 39 90 78 68 78 23"
+          ></polygon>
+          <text
+            x="46%"
+            y="50%"
+            textAnchor="middle"
+            dominantBaseline="middle"
+            fontSize="48"
+            fill="#64ffda"
+          >
+            J
+          </text>
+        </g>
+      </g>
+    </svg>
+  );
+}
