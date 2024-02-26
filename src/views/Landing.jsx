1827a4a3d86d4fb0e906b9bc02b7923a0da60108
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-26 18:51:38 -0300

    Update Landing.jsx

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import { Link } from "react-scroll";
+import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Projects from "../components/Project";
-import Contact from "../components/Contact";
 
 export default function Landing() {
-  const [es, setEs] = useState(false);
-
-  const changeLenguage = () => {
-    setEs(!es);
-  };
   return (
-    <div className="text-white w-full h-full bg-[#282828]">
-      <Header changeLenguage={changeLenguage} es={es} />
-      <Hero es={es} />
-      <About es={es} />
-      <Skills es={es} />
-      <Projects es={es} />
-      <Contact es={es} />
-    </div>
+    <>
+      <div>
+        <Header />
+        <Hero />
+        <About />
+      </div>
+      <div className="flex items-center justify-center">
+        <Link
+          className="hover:text-[--green] transition-all duration-500 mb-5"
+          activeClass="active"
+          to="header"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          <span className="text-[--green]"></span>up
+        </Link>
+      </div>
+    </>
   );
 }
