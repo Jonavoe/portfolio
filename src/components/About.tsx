ea6634b995a9f14d30456c0ac0fdefaba7a9b392
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-26 18:51:12 -0300

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div id='about' className='flex items-center justify-center my-96'>
+
+            <div className='w-96 h-96 border-2'>About</div>
+        </div>
+    )
+}
