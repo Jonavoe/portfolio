91f0d61e56c12ed8f64ff009e565ca5b1b7f4f5e
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-27 19:58:24 -0300

    Create LogoBack.tsx

---
+++ src/components/icons/LogoBack.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function LogoBack() {
+    return (
+        <svg
+            id="logo"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            viewBox="0 0 84 96"
+        >
+            <title>Logo</title>
+            <g transform="translate(-8.000000, -2.000000)">
+                <g transform="translate(11.000000, 5.000000)">
+
+                    <polygon
+                        id="Shape"
+                        fill="#64ffda"
+                        stroke="#64ffda"
+                        strokeWidth="5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        points="39 0 0 22 0 67 39 90 78 68 78 23"
+                    ></polygon>
+
+                </g>
+            </g>
+        </svg>
+    );
+}
