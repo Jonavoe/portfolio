a29c2966b458080f518f9798306c2d987cfa1988
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-27 19:58:16 -0300

    Create Experience.tsx

---
+++ src/components/Experience.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Experience() {
+    return (
+        <div id='experience' className='flex justify-center my-60'>
+            <div className='w-3/4 md:w-3/6'>
+                Experience
+            </div>
+        </div>
+    )
+}
