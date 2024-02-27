f8d371ac3c2101e20de83643b34687d095c0b7a7
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-27 16:12:30 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import FotoCv from "../assets/Imagen/FotoCv.jpg";
 
 export default function About() {
     return (
-        <div id='about' className='flex items-center justify-center my-96'>
+        <div id='about' className='flex justify-center  gap-10 my-44'>
+            <div className='w-3/4 md:w-3/6 flex  justify-center gap-10'>
+                <div className='flex w-3/4 flex-col gap-4 '>
 
-            <div className='w-96 h-96 border-2'>About</div>
-        </div>
+                    <h2 className='text-4xl font-bold text-[--lightest-slate]'><span className='text-[--green]'>01.</span> About Me</h2>
+                    <p className=' text-[--light-slate]'>Hello! I'm Jonathan Voeffray, a web developer with a passion for creating digital solutions. My journey in web development began in 2021 when I started learning HTML, CSS, and React.</p>
+                    <p className='text-[--light-slate]'>Since then, I've gained experience at two companies: one specializing in <a className='text-[--green] hover-underline-animation' href="https://www.socialmedialab.ai/" target='_blank'>SML</a> applying artificial intelligence to social media and the other in automating processes for car dealerships, called <a className='text-[--green] hover-underline-animation' href="https://www.socialmedialab.ai/" target='_blank'>Autoflow</a>.</p>
+                    <p className='text-[--light-slate]'>My current focus is on creating accessible and inclusive digital products for diverse clients. Here are some of the technologies I've been working with:</p>
+                    <ul className='grid grid-cols-2 grid-rows-3 gap-4 text-[--light-slate]'>
+                        <li><span className='text-[--green]'>▹</span> JavaScript (ES6+)</li>
+                        <li><span className='text-[--green]'>▹</span> React</li>
+                        <li><span className='text-[--green]'>▹</span> Next</li>
+                        <li><span className='text-[--green]'>▹</span> Node.js</li>
+                        <li><span className='text-[--green]'>▹</span> TypeScript</li>
+                        <li><span className='text-[--green]'>▹</span> WordPress</li>
+                    </ul>
+
+                </div>
+                <div>
+                    <img className='w-72 mt-14' src={FotoCv} alt="profile" />
+                </div>
+            </div>
+        </div >
     )
 }
