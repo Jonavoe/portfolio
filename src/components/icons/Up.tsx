78adaf6375745edadc8da16962539648ed078c88
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-02-28 17:48:33 -0300

    Create Up.tsx

---
+++ src/components/icons/Up.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Up() {
+    return (
+        <div className='w-14 h-6'>
+            <svg fill="a8b2d1" viewBox="0 0 24 24" id="up-direction-2" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" className="icon line-color">
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <polyline id="secondary" points="18.17 17.71 12 11.54 5.83 17.71" style={{ fill: 'none', stroke: '#64ffda', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: 2 }}></polyline>
+                    <path id="primary" d="M5.83,17.71,3,14.89l8.29-8.3a1,1,0,0,1,1.42,0L21,14.88l-2.83,2.83" style={{ fill: 'none', stroke: '#a8b2d1', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: 2 }}></path>
+                </g>
+            </svg>
+        </div>
+    );
+}
