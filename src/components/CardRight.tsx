bee4c483b8c2d479019fbcf9221fe781d443a8e2
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-05 15:31:58 -0300

    Create CardRight.tsx

---
+++ src/components/CardRight.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import GithubIcons from './icons/GithubIcons'
+import Share from './icons/Share'
+import { Link } from 'react-router-dom'
+
+export default function CardRight({ image, title, description, technologies, git, page }) {
+    return (
+        <div className='relative flex justify-end items-end w-full h-full mb-14'>
+            <div className='absolute w-7/12 top-0 left-0 z-0'>
+                <div className='div-1 z-50 absolute w-full h-full'></div>
+                <img className='z-40' src={image} alt="profile" />
+            </div>
+            <div className='w-1/2 z-10' >
+                <div className='flex flex-col justify-end items-end gap-3'>
+                    <div className='text-[--green]'>
+                        Featured Project
+                    </div>
+                    <div className='text-[--lightest-slate] text-[1.5rem] font-semibold transition duration-300 hover:text-[--green] hover-underline-animation-work'>
+                        {title}
+                    </div>
+                    <div className='py-3 px-4 bg-[--light-navy] text-[--light-slate] text-end rounded-md'>
+                        {description}
+                    </div>
+                    <div className='flex gap-2 text-[--slate]'>
+                        {technologies && technologies.map(tech =>
+                            < div className='transition duration-300 hover:text-[--green]' > {tech}</div>
+                        )}
+                    </div>
+                    <div className='flex gap-3 '>
+                        <Link className='w-6' to={git} target='_blank'>
+                            <GithubIcons />
+                        </Link>
+
+                        <Link className='w-6' to={page} target='_blank'>
+                            <Share />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
