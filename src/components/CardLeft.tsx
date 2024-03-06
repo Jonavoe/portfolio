60ff3789b4c969ac3662eea4ae27eca474dc3277
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 13:19:41 -0300

    Update CardLeft.tsx

--- src/components/CardLeft.tsx
+++ src/components/CardLeft.tsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 export default function CardLeft({ image, title, description, technologies, git, page }) {
     return (
         <div className='relative flex justify-end w-full h-full mb-14'>
-            <div className='absolute w-7/12 z-0'>
-                <div className='div-1 z-50 absolute w-full h-full'></div>
-                <img className='z-40' src={image} alt="profile" />
+            <div className='absolute w-7/12 z-0 rounded-md'>
+                <div className='div-1 z-50 absolute w-full h-full rounded-md'></div>
+                <img className='z-40 rounded-md' src={image} alt="profile" />
             </div>
             <div className='flex justify-start items-start'>
                 <div className='flex w-1/2 z-10'>
@@ -19,22 +19,26 @@ export default function CardLeft({ image, title, description, technologies, git,
                         <div className='text-[--lightest-slate] text-[1.5rem] font-semibold transition duration-300 hover:text-[--green] hover-underline-animation-work'>
                             {title}
                         </div>
-                        <div className='py-3 px-4 bg-[--light-navy] text-[--light-slate] text-start rounded-md'>
+                        <div className='py-3 px-4 text-sm bg-[--light-navy] text-[--light-slate] text-start rounded-md'>
                             {description}
                         </div>
                         <div className='flex gap-2 text-[--slate]'>
                             {technologies && technologies.map(tech =>
-                                < div className='transition duration-300 hover:text-[--green]' > {tech}</div>
+                                < div className='transition duration-300 text-sm hover:text-[--green]' > {tech}</div>
                             )}
                         </div>
                         <div className='flex gap-3 '>
-                            <Link className='w-6' to={git} target='_blank'>
-                                <GithubIcons />
-                            </Link>
+                            {git !== "" &&
+                                <Link className='w-6' to={git} target='_blank'>
+                                    <GithubIcons />
+                                </Link>
+                            }
 
-                            <Link className='w-6' to={page} target='_blank'>
-                                <Share />
-                            </Link>
+                            {page !== "" &&
+                                <Link className='w-6' to={page} target='_blank'>
+                                    <Share />
+                                </Link>
+                            }
                         </div>
                     </div>
                 </div>
