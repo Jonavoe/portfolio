e99e9d475f41bf44895585c05a312a112d339e4c
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 18:42:04 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Logo from "./icons/Logo";
 import { Link, animateScroll as scroll } from "react-scroll";
 import LogoBack from './icons/LogoBack';
+import arg from '../assets/argentina.jpg'
+import us from '../assets/us.jpg'
 
 
-export default function Header() {
+export default function Header({ es, handleLenguage }) {
     return (
         <div id='header' className=' justify-between items-center mx-10 my-4 header flex'>
             <div className='w-14 h-14 relative cursor-pointer'>
@@ -16,7 +18,8 @@ export default function Header() {
                 </div>
             </div>
 
-            <div className="gap-8  hidden md:flex">
+            <div className="gap-8 items-center justify-center hidden md:flex">
+
                 <ul className='flex items-center justify-center gap-8'>
                     <li>
                         <Link
@@ -28,7 +31,7 @@ export default function Header() {
                             offset={-70}
                             duration={500}
                         >
-                            <span className="text-[--green]">01. </span>About
+                            <span className="text-[--green]">01. </span>{es ? "Sobre mí" : "About"}
                         </Link>
                     </li>
                     <li>
@@ -41,7 +44,7 @@ export default function Header() {
                             offset={-70}
                             duration={500}
                         >
-                            <span className="text-[--green]">02. </span>Experience
+                            <span className="text-[--green]">02. </span>{es ? "Experiencia" : "Experience"}
                         </Link>
                     </li>
                     <li>
@@ -54,7 +57,7 @@ export default function Header() {
                             offset={-70}
                             duration={500}
                         >
-                            <span className="text-[--green]">03. </span>Work
+                            <span className="text-[--green]">03. </span>{es ? "Proyectos" : "Work"}
                         </Link>
                     </li>
                     <li>
@@ -67,13 +70,22 @@ export default function Header() {
                             offset={-70}
                             duration={500}
                         >
-                            <span className="text-[--green]">04. </span>Contact
+                            <span className="text-[--green]">04. </span>{es ? "Contacto" : "Contact"}
                         </Link>
                     </li>
                 </ul>
-                <a className="border rounded-sm border-[--green] px-2 py-1 cursor-pointer hover:text-[--green]" href='https://drive.google.com/file/d/12FmML7tNOXSdCl0lCtTYDBZE7YyYvMvp/view?usp=sharing' target='_blank'>
-                    Resume
+                <a className="border rounded-sm border-[--green] px-2 py-1 cursor-pointer hover:text-[--green]" href={`${es ? "https://drive.google.com/file/d/19xLz9cup3NS1R7cB9P5kUHDrLa6lT5f_/view?usp=sharing" : "https://drive.google.com/file/d/12FmML7tNOXSdCl0lCtTYDBZE7YyYvMvp/view?usp=sharing"}`} target='_blank'>
+                    {es ? "Resumen" : "Resume"}
                 </a>
+                {es ?
+                    <div className="w-10" onClick={() => handleLenguage()}>
+                        <img src={us} alt="" />
+                    </div>
+                    :
+                    <div className="w-10" onClick={() => handleLenguage()}>
+                        <img src={arg} alt="" />
+                    </div>
+                }
             </div>
         </div>
     )
