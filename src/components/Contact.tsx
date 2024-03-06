e22edfedd1e95c19543d143d6b35b540c4c16d97
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 17:05:10 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
         <div id='contact' className='flex justify-center items-center gap-10 md:mt-20 md:mb-20'>
             <div className=' flex justify-center gap-4 md:gap-10'>
                 <div className='flex w-3/4 md:w-4/12 justify-center items-center flex-col gap-8 '>
-                    <h2 className='text-lg font-bold text-[--lightest-slate]'><span className='text-[--green]'>01.</span> What’s Next?</h2>
+                    <h2 className='text-lg font-bold text-[--lightest-slate]'><span className='text-[--green]'>04.</span> What’s Next?</h2>
                     <p className='text-4xl font-bold text-[--lightest-slate]'>Get In Touch</p>
                     <p className='text-[--light-slate] text-center'>I am currently seeking new professional opportunities. If you have any offers or simply want to get in touch with me, feel free to do so! I am open to new opportunities and would be happy to hear from you!</p>
 
