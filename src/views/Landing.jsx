bf53aa266426c92a4eb13fa71a8f4acb3b7476f0
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 16:23:54 -0300

    Update Landing.jsx

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import Experience from "../components/Experience";
 import Up from "../components/icons/Up";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export default function Landing() {
   return (
@@ -15,6 +16,7 @@ export default function Landing() {
         <About />
         <Experience />
         <Projects />
+        <Contact />
       </div>
       <div className="flex items-center justify-center ">
         <Link
