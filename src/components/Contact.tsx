24f42c905b0d1d6ae8e4b7a8320d925259a19b41
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 16:57:02 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,9 +3,9 @@ import FotoCv from "../assets/FotoCv.jpg";
 
 export default function Contact() {
     return (
-        <div id='contact' className='flex justify-center items-center gap-10 mt-20 mb-20'>
-            <div className=' flex justify-center gap-10'>
-                <div className='flex w-4/12 justify-center items-center flex-col gap-8 '>
+        <div id='contact' className='flex justify-center items-center gap-10 md:mt-20 md:mb-20'>
+            <div className=' flex justify-center gap-4 md:gap-10'>
+                <div className='flex w-3/4 md:w-4/12 justify-center items-center flex-col gap-8 '>
                     <h2 className='text-lg font-bold text-[--lightest-slate]'><span className='text-[--green]'>01.</span> What’s Next?</h2>
                     <p className='text-4xl font-bold text-[--lightest-slate]'>Get In Touch</p>
                     <p className='text-[--light-slate] text-center'>I am currently seeking new professional opportunities. If you have any offers or simply want to get in touch with me, feel free to do so! I am open to new opportunities and would be happy to hear from you!</p>
