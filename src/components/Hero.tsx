baa26665e838ecf1b24289182f6fd3c9ea3b27a1
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-06 16:57:09 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div id='hero' className='flex justify-center my-60'>
+        <div id='hero' className='flex justify-center my-20 md:my-60'>
             <div className='w-3/4 md:w-3/6'>
-                <div className='flex flex-col gap-6'>
+                <div className='flex flex-col gap-2 md:gap-6'>
                     <p className='text-[--green]   font-medium'>
                         Hi, my name is
                     </p>
-                    <h1 className='text-[--lightest-slate] text-7xl  font-semibold'>
+                    <h1 className='text-[--lightest-slate] text-2xl md:text-7xl  font-semibold'>
                         Jonathan Voeffray.
                     </h1>
-                    <h2 className='text-[--light-slate] text-7xl font-medium'>
+                    <h2 className='text-[--light-slate] text-2xl md:text-7xl font-medium'>
                         I build things for the web.
                     </h2>
                     <p className='w-3/4 text-[--slate]'>
