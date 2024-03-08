19e5dcf8e39bed2c0e0e766921b9f4dc3b105a58
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-03-08 14:06:48 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import FotoCv from "../assets/FotoCv.jpg";
 
-export default function Contact() {
+export default function Contact({ es }) {
     return (
         <div id='contact' className='flex justify-center items-center gap-10 md:mt-20 md:mb-20'>
             <div className=' flex justify-center gap-4 md:gap-10'>
                 <div className='flex w-3/4 md:w-4/12 justify-center items-center flex-col gap-8 '>
-                    <h2 className='text-lg font-bold text-[--lightest-slate]'><span className='text-[--green]'>04.</span> What’s Next?</h2>
-                    <p className='text-4xl font-bold text-[--lightest-slate]'>Get In Touch</p>
-                    <p className='text-[--light-slate] text-center'>I am currently seeking new professional opportunities. If you have any offers or simply want to get in touch with me, feel free to do so! I am open to new opportunities and would be happy to hear from you!</p>
+                    <h2 className='text-lg font-bold text-[--lightest-slate]'><span className='text-[--green]'>04.</span> {es ? "¿Cuál es el siguiente paso?" : "What’s Next?"}</h2>
+                    <p className='text-4xl font-bold text-[--lightest-slate]'>{es ? "No dudes en contactarme" : "Get In Touch"}</p>
+                    <p className='text-[--light-slate] text-center'>{es ? "Actualmente estoy buscando nuevas oportunidades profesionales. Si tienes alguna oferta o simplemente quieres ponerte en contacto conmigo, ¡no dudes en hacerlo! Estoy abierto a nuevas oportunidades y me encantaría saber de ti." : "I am currently seeking new professional opportunities. If you have any offers or simply want to get in touch with me, feel free to do so! I am open to new opportunities and would be happy to hear from you!"}</p>
 
 
                     <a
@@ -17,7 +17,7 @@ export default function Contact() {
                         target='_blank'
                         rel="noreferrer"
                     >
-                        Say Hello
+                        {es ? "¡Hola!" : "Say Hello"}
                     </a>
 
                 </div>
